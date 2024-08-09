6163a8a3a766374beffb480300d19bbebf0cf4ed
Abi <abitha302001@gmail.com>
2024-08-09 22:34:19 +0100

    feat: more stuff

--- common/webSockets/webSocketsHelper.ts
+++ common/webSockets/webSocketsHelper.ts
@@ -8,7 +8,7 @@ export async function fetchTokenAndStartConnection() {
     throw new Error("Token not found");
   }
   const connection = new HubConnectionBuilder()
-    .withUrl("http://192.168.1.92:5000" + "/chatHub", {
+    .withUrl("https://wecreatex.azurewebsites.net" + "/chatHub", {
       accessTokenFactory: () => token,
     })
     .withAutomaticReconnect()
