d5be1c82a3354732a3737fc91e9eaace7bc47235
Abi <abitha302001@gmail.com>
2025-01-10 00:02:28 +0000

    feat: added git stuff

--- api/enum/backEndErrors.ts
+++
@@ -1,8 +0,0 @@
-enum BackEndErrors {
-  USERNAME_NOT_FOUND = "username-not-found",
-  INVALID_PASSWORD = "invalid-password",
-  INVALID_VOUCHER_CODE = "invalid-voucher-code",
-  SAME_PASSWORD = "same-password",
-}
-
-export default BackEndErrors;
--- api/enum/secureStoreKeys.ts
+++
@@ -1,9 +0,0 @@
-enum SecureStoreKeys {
-    USER_TOKEN = 'userToken',
-    DATABASE_KEY = 'databaseKey',
-    PRIVATE_KEY = 'we-create-privatekey',
-    PUBLIC_KEY = 'we-create-publicKey',
-    COMPLETED_20_AUGUST_SURVEY = 'completed20AugustSurvey',
-}
-
-export default SecureStoreKeys;
